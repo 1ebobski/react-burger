8d152dc0f48061d310921cee5b3fd3cb709fd379
Fedor Lukhmanov <fedor.lukhmanov@ya.ru>
2022-08-24 18:34:27 +0300

    small tweak to redirect to homepage from gh-pages default page

--- src/pages/not-found-page.tsx
+++ src/pages/not-found-page.tsx
@@ -1,12 +1,34 @@
 import notFoundStyles from "./styles/not-found.module.css";
+import { Redirect, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
+interface ILocation extends Location {
+  background: ILocation;
+  state: ILocation | undefined;
+}
 
 export default function NotFoundPage(): JSX.Element {
-  return (
+  const location = useLocation<ILocation>();
+  const { pathname } = location;
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  return pathname !== "/react-burger" ? (
     <div className={notFoundStyles.container}>
       <span className='text text_type_digits-large'>404</span>
       <span className='mt-4 text text_type_main-medium'>
         cтраница не найдена
       </span>
     </div>
+  ) : (
+    // small tweak to redirect to homepage from gh-pages default page
+    <Redirect
+      to={{
+        pathname: "/",
+        state: { from: location },
+      }}
+    />
   );
 }
